c9310681d428cca3e52143734c2e029a82d6e997
Rosalba Monterrosas <43211322+rosalbamonterrosas@users.noreply.github.com>
2018-10-22 15:43:52 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -24,7 +24,7 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
-MAIN	mov.b #-128, R4
+MAIN		mov.b #-128, R4
 		mov.b #1, R5
 		call #MULT
 
@@ -35,35 +35,37 @@ MAIN	mov.b #-128, R4
 Mainloop jmp Mainloop
 
 
-MULT	clr R8
+MULT		clr R8
 		mov.b R4, R9 ; preserve value in R4
 		cmp.b #0, R4
 		jz store ; check if zero
 		jl check ; check if negative and then check the next register
-		inv.b R4 ; 1’s complement
-		add.b #1, R4  ; 2’s complement to change positive value to negative
+		inv.b R4 ; 1â€™s complement
+		add.b #1, R4  ; 2â€™s complement to change positive value to negative
 
-check	mov.b R5, R10 ; preserve value in R5
+check		mov.b R5, R10 ; preserve value in R5
 		cmp.b #0, R5
 		jz store ; check if zero
 		jl compare ; check if negative
-		inv.b R5 ; 1’s complement
-		add.b #1, R5  ; 2’s complement to change positive value to negative
+		inv.b R5 ; 1â€™s complement
+		add.b #1, R5  ; 2â€™s complement to change positive value to negative
 
-compare	cmp.b R5, R4
+compare		cmp.b R5, R4
 		jl loop
 		mov.b R4, R7 ; R7 is used for temporary storage for swapping R5 and R4
 		mov.b R5, R4
 		mov.b R7, R5
-loop	add.b R5, R8
+		
+loop		add.b R5, R8
 		dec.b R4
 		jnz loop
 		xor.b R9, R10 ; check if both values are positive or negative
 		bit.b #80h, R10
 		jz store
-		inv.b R8 ; 1’s complement of final result
-		add.b #1, R8  ; 2’s complement of final result
-store	mov.b R8, &0x020A
+		inv.b R8 ; 1â€™s complement of final result
+		add.b #1, R8  ; 2â€™s complement of final result
+		
+store		mov.b R8, &0x020A
 		ret
 
 
@@ -74,28 +76,29 @@ DIV		clr R7
 		cmp.b #0, R6
 		jz store2 ; check if zero
 		jl check2 ; check if negative and then check the next register
-		inv.b R6 ; 1’s complement
-		add.b #1, R6  ; 2’s complement to change positive value to negative
+		inv.b R6 ; 1â€™s complement
+		add.b #1, R6  ; 2â€™s complement to change positive value to negative
 
-check2	mov.b R5, R10 ; preserve value in R5
+check2		mov.b R5, R10 ; preserve value in R5
 		cmp.b #0, R5
 		jl loop2 ; check if negative
-		inv.b R5 ; 1’s complement
-		add.b #1, R5  ; 2’s complement to change positive value to negative
+		inv.b R5 ; 1â€™s complement
+		add.b #1, R5  ; 2â€™s complement to change positive value to negative
 
-loop2	inc.b R7 ; R7 is counter
+loop2		inc.b R7 ; R7 is counter
 		sub.b R5, R6
 		jn loop2
 		jz loop2
 		dec.b R7
 
-		xor.b R9, R10
+		xor.b R9, R10 ; check if both values are positive or negative
 		bit.b #80h, R10
 		jz store2
-		inv.b R7 ; 1’s complement of final result
-		add.b #1, R7  ; 2’s complement of final result
+		inv.b R7 ; 1â€™s complement of final result
+		add.b #1, R7  ; 2â€™s complement of final result
 
-store2	mov.b R7, &0x021F
+store2		mov.b R7, &0x021F
+
 end		ret
                                             
 
